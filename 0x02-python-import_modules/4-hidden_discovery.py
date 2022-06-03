ca497da757a99ccac08c7075deae0f870144d862
Teddy <teddylnk1@gmail.com>
2022-06-03 08:15:54 +0300

    bug fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,4 +5,5 @@ import hidden_4
 if __name__ == "__main__":
     for content in dir(hidden_4):
         if content[:2] != "__":
-            print("{:s}".format(content))
+            print("{:s}".format(content)) 
+            
